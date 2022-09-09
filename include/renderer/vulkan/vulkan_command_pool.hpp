13465bcf0b5fad2b5ba8f4cd6d84b0dfb84e8754
android172 <63978434+android172@users.noreply.github.com>
2022-09-09 14:18:36 +0200

    Command buffer code annotation added.

--- include/renderer/vulkan/vulkan_command_pool.hpp
+++ include/renderer/vulkan/vulkan_command_pool.hpp
@@ -22,9 +22,25 @@ public:
     );
     ~VulkanCommandPool();
 
-    vk::CommandBuffer allocate_command_buffer();
-    std::vector<vk::CommandBuffer> allocate_command_buffers(uint32 size);
+    /// @brief Allocate vulkan command buffer from the pool
+    /// @param primary Specify whether the created buffer is a primary buffer
+    /// @returns Created and allocated buffer
+    vk::CommandBuffer allocate_command_buffer(bool primary = true);
+    /// @brief Allocate vulkan command buffers from the pool
+    /// @param primary Specify whether created buffers are primary buffers
+    /// @returns Created and allocated buffers
+    std::vector<vk::CommandBuffer> allocate_command_buffers(uint32 size, bool primary = true);
+    /// @brief Returns command buffer to the pool
+    /// @param command_buffer Buffer to free
+    void free_command_buffer(vk::CommandBuffer command_buffer);
+    /// @brief Returns command buffers to the pool
+    /// @param command_buffers Buffers to free
+    void free_command_buffers(std::vector<vk::CommandBuffer> command_buffers);
 
+    /// @brief Allocate and begin a single use buffer
+    /// @returns Created buffer
     vk::CommandBuffer begin_single_time_commands();
+    /// @brief End and free a single use buffer
+    /// @param command_buffer Buffer to free
     void end_single_time_commands(vk::CommandBuffer command_buffer);
 };
--- src/renderer/vulkan/vulkan_command_pool.cpp
+++ src/renderer/vulkan/vulkan_command_pool.cpp
@@ -7,8 +7,11 @@ VulkanCommandPool::VulkanCommandPool(
     const vk::Queue* queue,
     const uint32 queue_index
 ) : _device(device), _allocator(allocator), _queue(queue) {
+    // Create vulkan command pool
     vk::CommandPoolCreateInfo command_pool_info{};
+    // Allows for separate reset of buffers from the pool
     command_pool_info.setFlags(vk::CommandPoolCreateFlagBits::eResetCommandBuffer);
+    // The queue to which pull command buffers will be submitted to
     command_pool_info.setQueueFamilyIndex(queue_index);
 
     try {
@@ -20,28 +23,27 @@ VulkanCommandPool::~VulkanCommandPool() {
     _device->destroyCommandPool(_command_pool, _allocator);
 }
 
+// /////////////// //
+// Private methods //
+// /////////////// //
+
+
+
 // ////////////// //
 // Public methods //
 // ////////////// //
-vk::CommandBuffer VulkanCommandPool::allocate_command_buffer() {
-    vk::CommandBufferAllocateInfo alloc_info{};
-    alloc_info.setCommandPool(_command_pool);
-    alloc_info.setLevel(vk::CommandBufferLevel::ePrimary);
-    alloc_info.setCommandBufferCount(1);
-
-    vk::CommandBuffer command_buffer;
-    try {
-        command_buffer = _device->allocateCommandBuffers(alloc_info)[0];
-    } catch (const vk::SystemError& e) { Logger::fatal(e.what()); }
-
-    return command_buffer;
+vk::CommandBuffer VulkanCommandPool::allocate_command_buffer(bool primary) {
+    return allocate_command_buffers(1, primary)[0];
 }
 
-std::vector<vk::CommandBuffer> VulkanCommandPool::allocate_command_buffers(uint32 size) {
+std::vector<vk::CommandBuffer> VulkanCommandPool::allocate_command_buffers(uint32 size, bool primary) {
+    // Allocate command buffers from the pool
     vk::CommandBufferAllocateInfo alloc_info{};
-    alloc_info.setCommandPool(_command_pool);
-    alloc_info.setLevel(vk::CommandBufferLevel::ePrimary);
-    alloc_info.setCommandBufferCount(size);
+    alloc_info.setCommandPool(_command_pool);   // Pool reference
+    alloc_info.setCommandBufferCount(size);     // Number of buffers to be created
+    // Command buffer level. Secondary command buffers cannot be submitted directly to the device queue,
+    // Instead they are used by primary command buffers.
+    alloc_info.setLevel(primary ? vk::CommandBufferLevel::ePrimary : vk::CommandBufferLevel::eSecondary);
 
     std::vector<vk::CommandBuffer> command_buffers;
     try {
@@ -51,18 +53,21 @@ std::vector<vk::CommandBuffer> VulkanCommandPool::allocate_command_buffers(uint3
     return command_buffers;
 }
 
+void VulkanCommandPool::free_command_buffer(vk::CommandBuffer command_buffer) {
+    _device->freeCommandBuffers(_command_pool, 1, &command_buffer);
+}
+
+void VulkanCommandPool::free_command_buffers(std::vector<vk::CommandBuffer> command_buffers) {
+    _device->freeCommandBuffers(_command_pool, command_buffers);
+}
+
 vk::CommandBuffer VulkanCommandPool::begin_single_time_commands() {
-    vk::CommandBufferAllocateInfo allocation_info{};
-    allocation_info.setLevel(vk::CommandBufferLevel::ePrimary);
-    allocation_info.setCommandBufferCount(1);
-    allocation_info.setCommandPool(_command_pool);
+    vk::CommandBuffer command_buffer = allocate_command_buffer();
 
-    vk::CommandBuffer command_buffer;
-    command_buffer = _device->allocateCommandBuffers(allocation_info)[0];
-
-    // Begin recording commands
+    // Begin recording commands (indicating the single use property)
     vk::CommandBufferBeginInfo begin_info{};
     begin_info.setFlags(vk::CommandBufferUsageFlagBits::eOneTimeSubmit);
+
     command_buffer.begin(begin_info);
 
     return command_buffer;
@@ -81,5 +86,5 @@ void VulkanCommandPool::end_single_time_commands(vk::CommandBuffer command_buffe
     _queue->waitIdle();
 
     // Free temp command buffer
-    _device->freeCommandBuffers(_command_pool, 1, &command_buffer);
+    free_command_buffer(command_buffer);
 }
